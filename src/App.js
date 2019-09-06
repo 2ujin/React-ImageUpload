ce443822a75d80a804f9cf529bd2a994b09141b7
2ujin <s2017i13@e-mirim.hs.kr>
2019-09-07 00:56:01 +0900

    로고 추가 및 css 수정

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import React from 'react';
-import ImageUploader from 'react-images-upload';
+import ImagesUploader  from 'react-images-upload';
 import './App.css';
 
 export default class App extends React.PureComponent {
+  state = {
+    counter : 0
+  }
+  ondrop= (e) => {
+		this.setState({
+      counter : this.state.counter + 1
+    });
+  }      
   render() {
     return (
-        <div className="page">
-            <ImageUploader style={{ 
-              maxWidth: '500px', 
-              margin: "20px auto"
-            }}
+      <main className="image-upload">
+          <img src="https://studio.stipop.io/static/images/logo-pink.svg" className="title" />
+          <img src="https://studio.stipop.io/static/images/tag-studio.png" className="studio"/>
+          <div className="page">
+            <div className="info">정보다</div>
+            <ImagesUploader className="imageuploader"
               withIcon={false}
               withPreview={true}
               withLabel={false}
               buttonText='Upload!'
               onChange={this.onDrop}
               imgExtension={['.png', '.gif']}
-              fileTypeError={''}
+              fileTypeError={'png와 gif로 올려주세요 :-)'}
+              fileSizeError={'500kb 이하로 올려주세요 :-)'}
               maxFileSize={500000}
-              
+              color={'#142435'}
+              onChange={this.ondrop}
+              singleImage={true}
               />
+              {this.state.counter}
         </div>
+        </main>
     );
+
 }
 
 }
