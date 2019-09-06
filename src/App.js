845624dca97b856c4e9bd01dd46992fca7d4e288
2ujin <s2017i13@e-mirim.hs.kr>
2019-09-06 20:51:58 +0900

    리액트설치 및  환경설정

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div>
+        <input type="file" name="file" onChange={null}/>
+        <button type="button" onClick={null}>눌러봐랑</button>
+      </div>
+  );
+}
+
+export default App;
