1e2d24b226978bfd11ff6361d8d8e32522ad9a2e
2ujin <s2017i13@e-mirim.hs.kr>
2019-09-06 22:19:13 +0900

    웹 아이콘 수정

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  
   return (
       <div>
         <input type="file" name="file" onChange={null}/>
-        <button type="button" onClick={null}>블라블라블라블라</button>
+        <button type="button" onClick={null}>upload</button>
       </div>
   );
 }
