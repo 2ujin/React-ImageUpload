87a16291098eb582d2c735af371f9d2f120a2f56
2ujin <s2017i13@e-mirim.hs.kr>
2019-09-06 23:49:25 +0900

    imageuploder 사용해서 이미지 업로드

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import ImageUploader from 'react-images-upload';
 import './App.css';
 
-function App() {
-
-  
-  return (
-      <div>
-        <input type="file" name="file" onChange={null}/>
-        <button type="button" onClick={null}>upload</button>
-      </div>
-  );
+export default class App extends React.PureComponent {
+  render() {
+    return (
+        <div className="page">
+            <ImageUploader style={{ 
+              maxWidth: '500px', 
+              margin: "20px auto"
+            }}
+              withIcon={false}
+              withPreview={true}
+              withLabel={false}
+              buttonText='Upload!'
+              onChange={this.onDrop}
+              imgExtension={['.png', '.gif']}
+              fileTypeError={''}
+              maxFileSize={500000}
+              
+              />
+        </div>
+    );
 }
 
-export default App;
+}
