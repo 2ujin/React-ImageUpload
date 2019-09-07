5f082658bb45c4afd7dcd36c8428f74e3cf8b6e2
2ujin <s2017i13@e-mirim.hs.kr>
2019-09-07 14:49:34 +0900

    input file로 이미지 업로드 하기 (여러 개 가능)

--- src/App.js
+++ src/App.js
@@ -1,42 +1,56 @@
 import React from 'react';
-import ImagesUploader  from 'react-images-upload';
 import './App.css';
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
 
 export default class App extends React.PureComponent {
-  state = {
-    counter : 0
+
+  constructor() {
+    super();
+    
+    this.displayData = [];
+
+    this.state = {
+      showdata : this.displayData,
+      postVal : ""
+    }
+    this.appendData = this.appendData.bind(this);
+  };
+
+appendData=event => {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+  const url = reader.readAsDataURL(file);
+
+  reader.onload = function (e) {
+    this.setState({
+        imgSrc: [reader.result]
+    })
+  }.bind(this);
+
+  console.log(this.state.imgSrc); // 첫 값은 undefinded가 나온다 ..
+  this.displayData.push(<div id="display-data"><img src={this.state.imgSrc}></img></div>);
   }
-  ondrop= (e) => {
-		this.setState({
-      counter : this.state.counter + 1
-    });
-  }      
+
   render() {
     return (
       <main className="image-upload">
-          <img src="https://studio.stipop.io/static/images/logo-pink.svg" className="title" />
-          <img src="https://studio.stipop.io/static/images/tag-studio.png" className="studio"/>
-          <div className="page">
-            <div className="info">정보다</div>
-            <ImagesUploader className="imageuploader"
-              withIcon={false}
-              withPreview={true}
-              withLabel={false}
-              buttonText='Upload!'
-              onChange={this.onDrop}
-              imgExtension={['.png', '.gif']}
-              fileTypeError={'png와 gif로 올려주세요 :-)'}
-              fileSizeError={'500kb 이하로 올려주세요 :-)'}
-              maxFileSize={500000}
-              color={'#142435'}
-              onChange={this.ondrop}
-              singleImage={true}
-              />
-              {this.state.counter}
-        </div>
-        </main>
+        <img src="https://studio.stipop.io/static/images/logo-pink.svg" className="title" />
+        <img src="https://studio.stipop.io/static/images/tag-studio.png" className="studio"/>
+          <div className="page"> 
+            <div id="info">
+                
+            </div>
+              <div id="mainContainer">
+                <div id="img-box">
+                  <input ref="file" type="file" className="button" onChange={this.appendData}/>
+                <div> 
+                  {this.displayData}
+                </div>
+                </div>
+            </div>
+          </div> 
+      </main>
     );
-
-}
-
-}
+  }
+  }
