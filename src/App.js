88583018e028a2fd09ee8fb391c0981f35ab703c
2ujin <s2017i13@e-mirim.hs.kr>
2019-09-07 18:49:19 +0900

    info 개발 끝

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ export default class App extends React.PureComponent {
   
   constructor() {
     super();
-    var color1 = '#F9F9F9';
-    var color2 = '#cfcfcf';
+    var color1 = '#ffffff';
+    var color2 = '#f2f2f2';
 
     this.displayData = [];
     this.state = {
@@ -36,8 +36,8 @@ appendData=event => {
   this.displayData.push(<div id="display-data"><img src={this.state.imgSrc}></img></div>);
   }
 click(){
-  var color1 = '#F9F9F9';
-  var color2 = '#cfcfcf';
+  var color1 = '#ffffff';
+  var color2 = '#f2f2f2';
   this.setState({
     showMe: false,
     showMe1: true,
@@ -46,8 +46,8 @@ click(){
   })
 }
 click2(){
-  var color1 = '#F9F9F9';
-  var color2 = '#cfcfcf';
+  var color1 = '#ffffff';
+  var color2 = '#f2f2f2';
   this.setState({
     showMe: true,
     showMe1: false, 
@@ -69,7 +69,42 @@ click2(){
             </div>
             <div id = "main">
             <div id="info">
-            띠로링
+              <table>
+                <tr>
+                  <td>스티커 이름<br></br></td>
+                  <td><input type="text" id="input"></input><br></br>
+                  <code>스티커 이름은 50자 이내여야하며 글자와 숫자만 사용할 수 있습니다.</code>
+                  </td>
+                  
+                </tr>
+                <tr>
+                  <td>캐릭터 명</td>
+                  <td><input type="text" id="input"></input><br></br>
+                  <code>영숫자만 사용하여 문자 이름 쓰기.</code></td>
+                </tr>
+                <tr>
+                  <td>카테고리</td>
+                  <td align="left" valign="middle"><button id="btn">Animation/Cartoon</button><button>Animals</button><button>Celebrity</button><button>Food/Drink</button>
+                  <br></br><button id="btn">Gag</button><button>Holloween</button><button>People</button><button>Pharase</button><button>Romance</button>
+                  <br></br><button id="btn">Sports</button><button>Etc.</button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>키워드</td>
+                  <td><input type="text" id="input"></input><br></br>
+                  <code>2 - 5 개의 키워드를 써주세요. 쉼표를 사용하여 키워드를 구분해주세요.</code></td>
+                </tr>
+                <tr>
+                  <td>언어</td>
+                  <td><input type="text" id="input" list="language"></input></td>
+                  <datalist id="language">
+                    <option value="컴퓨터공학과"></option>
+                    <option value="영어영문과"></option>
+                    <option value="경영학과"></option>
+                    <option value="사회체육과"></option>
+                  </datalist>
+                </tr>
+              </table>
             </div>
             {this.state.showMe? // 사진 업로드 
               <div id="mainContainer">
